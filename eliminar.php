5da5e5d835442bcd4610c74444ef00157e97e606
yamirpaez <111025310+yamirpaez@users.noreply.github.com>
2023-05-16 06:13:58 -0600

    Update eliminar.php
    eliminar

--- eliminar.php
+++ eliminar.php
@@ -1,6 +1,6 @@
 <?php
     $id = $_GET['id'];
-    $cnx = mysqli_connect("localhost", "root", "", "tiendaonline");
+    $cnx = mysqli_connect("18.223.110.185", "ubuntu", "", "tiendaonline");
     $sql = "DELETE FROM productos WHERE id = $id";
     mysqli_query($cnx, $sql);
     mysqli_close($cnx);
